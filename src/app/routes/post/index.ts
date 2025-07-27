28be5a98f270909bef13e671803bab8fb01ea723
JJPontes <16lj27@hotmail.com>
2025-07-27 20:24:06 -0300

    - Add validation for UUID POST

--- src/app/routes/post/index.ts
+++ src/app/routes/post/index.ts
@@ -3,9 +3,10 @@ import { Router } from 'express';
 import { authenticateToken } from '../../middlewares/auth/authenticationValidate';
 import { asyncHandler } from '../../../utils/asyncHandler';
 import { removeById } from '../../controllers/postController';
+import { validarUUID } from '../../middlewares/utils/validateUtils';
 
 const router = Router();
 
-router.delete('/:id', asyncHandler(authenticateToken), removeById);
+router.delete('/:id', asyncHandler(authenticateToken), validarUUID, removeById);
 
 export default router;
